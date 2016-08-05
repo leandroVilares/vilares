dbba20d30dc6b1c905309538e26cdd62461c0a54
Saulo Nardin Rocha <saulonardin@gmail.com>
2016-08-05 12:18:55 -0300

    Melhorias e correções na seleção de perfil.

--- src/main/java/com/ml/logistica/romaneio/RomaneioApplication.java
+++ src/main/java/com/ml/logistica/romaneio/RomaneioApplication.java
@@ -1,12 +1,33 @@
 package com.ml.logistica.romaneio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import javax.inject.Inject;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class RomaneioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RomaneioApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(RomaneioApplication.class);
+
+    @Inject
+    private Environment env;
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(RomaneioApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "External: \thttp://{}:{}\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
 }
--- src/main/java/com/ml/logistica/romaneio/config/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.ml.logistica.romaneio.config;
-
-/**
- * Created by saulo on 05/08/2016.
- */
-public class Constants {
-}
