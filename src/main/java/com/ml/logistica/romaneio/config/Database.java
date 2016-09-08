c0be1b254d1b3818778f0fc12ed26c02ee4457e2
Saulo Nardin Rocha <saulonardin@gmail.com>
2016-09-08 12:19:17 -0300

    Melhorias - Bug Fix

--- src/main/java/com/ml/logistica/romaneio/config/Database.java
+++ src/main/java/com/ml/logistica/romaneio/config/Database.java
@@ -1,4 +1,4 @@
-package com.ml.logistica.romaneio;
+package com.ml.logistica.romaneio.config;
 
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
